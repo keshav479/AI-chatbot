cd6263e1a9351e21635c69eeb7a53b28525a7d09
Your Name <you@example.com>
2024-08-10 23:02:18 +0530

    change in url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext.tsx'
 import {Toaster} from 'react-hot-toast'
 import axios from 'axios'
 
-axios.defaults.baseURL= "https://gpt-bot-backend.vercel.app/api/v1/user";//backednURL
+axios.defaults.baseURL= "https://gpt-bot-backend.vercel.app/api/v1";//backednURL
 axios.defaults.withCredentials= true;
 
 
