5a26bbad69eb21a710d343bed52f3cc93e8dc076
Your Name <you@example.com>
2024-08-11 11:19:49 +0530

    cors updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import app from "./app.js"
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
 import { connectToDatabase } from "./db/connection.js"
 config();
 
 const PORT = process.env.PORT || 3004;
+const exp = express();
+
+exp.use(cors({ origin: "*" })); 
+
 connectToDatabase().then(() => {
 
 app.listen(PORT, () => console.log("Server open and conected to database 👍"));
