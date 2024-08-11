c0685420b99da33baa744a373587b05a85e49773
Your Name <you@example.com>
2024-08-11 21:30:58 +0530

    dist updated

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import { config } from "dotenv";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
@@ -6,7 +6,8 @@ import cors from 'cors';
 config();
 const app = express();
 //middleware
-app.use(cors({ origin: "https://gpt-bot-gamma.vercel.app", credentials: true }));
+// app.use(cors({origin: "https://gpt-bot-gamma.vercel.app", credentials: true}));
+app.use(cors({ origin: "*" }));
 // app.use(cors());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
